ced92ef7e64a886629a2a1297dbef0e0b121b02a
Gagan-30 <109915066+Gagan-30@users.noreply.github.com>
2023-07-10 18:50:34 +0100

    Update Sha1Cracker.py

--- Sha1Cracker.py
+++ Sha1Cracker.py
@@ -4,7 +4,7 @@ from colorama import Fore
 
 sha1hash = input('[*] Enter SHA1 Hash: ')
 
-passwordList = str(urllib.request.urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-10000.txt').read(), 'UTF-8')
+passwordList = str(urllib.request.urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt').read(), 'UTF-8')
 
 for password in passwordList.split('\n'):
     hashGuess = hashlib.sha1(bytes(password, 'UTF-8')).hexdigest()
